f457e478824a8dd628ddf2a3ae206c1352f382f1
Pietro Castro <pietrocastrodatadev@gmail.com>
2024-08-25 01:40:45 -0300

    Tabela com os resultados de todos os experimentos

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+# estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Jogando uma moeda')
 
